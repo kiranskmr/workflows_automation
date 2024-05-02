ade3be2a726481d7268277f241a5f3c6f2707bf6
kiranskmr <105718255+kiranskmr@users.noreply.github.com>
2024-05-02 15:47:41 +0100

    updates

--- setup.py
+++ setup.py
@@ -1,8 +1,37 @@
+"""
+setup.py configuration script describing how to build and package this project.
+
+This file is primarily used by the setuptools library and typically should not
+be executed directly. See README.md for how to deploy, test, and run
+the dab_whl project.
+"""
 from setuptools import setup, find_packages
 
+import sys
+sys.path.append('./src')
+
+import datetime
+import dabwhl
 
 setup(
     name="workflows_automation",
-    version="1.0",
-    packages=find_packages(),
+    # We use timestamp as Local version identifier (https://peps.python.org/pep-0440/#local-version-identifiers.)
+    # to ensure that changes to wheel package are picked up when used on all-purpose clusters
+    version=dabwhl.__version__ ,
+    url="https://databricks.com",
+    author="kiran.sreekumar@databricks.com",
+    description="wheel file based on workflows_automation/src",
+    packages=find_packages(where='./src'),
+    package_dir={'': 'src'},
+    entry_points={
+        "packages": [
+            "main=dab_whl.__main__:main"
+        ]
+    },
+    install_requires=[
+        # Dependencies in case the output wheel file is used as a library dependency.
+        # For defining dependencies, when this package is used in Databricks, see:
+        # https://docs.databricks.com/dev-tools/bundles/library-dependencies.html
+        "setuptools"
+    ],
 )
--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -4,7 +4,7 @@ from databricks.connect import DatabricksSession
 import pandas as pd
 from datetime import datetime
 
-from src.utils import *
+from utils import *
 
 
 def test_lowercase_names():
@@ -75,43 +75,3 @@ def test_uppercase_names():
     pd.testing.assert_frame_equal(left=expected_output_df,right=output_df_as_pd, check_exact=True)
 
 
-def test_metadata():
-    # ASSEMBLE 
-    test_data = [
-        {
-            "id": 1,
-            "first_name": "Kiran",
-            "last_name": "Sreekumar",
-            "age": 35
-        },
-        {
-            "id": 2,
-            "first_name": "Sachin",
-            "last_name": "Tendulkar",
-            "age": 45
-        }
-    ]
-
-    now = datetime.now()
-    field_dict = {
-        "task_id": 1,
-        "ingested_at": now
-    }
-    spark = DatabricksSession.builder.profile("e2-demo-field-eng").getOrCreate()
-    test_df = spark.createDataFrame(map(lambda x: Row(**x), test_data))
-
-    # ACT 
-    output_df = metadata(df=test_df, field_dict=field_dict)
-
-    output_df_as_pd = output_df.toPandas()
-
-    expected_output_df = pd.DataFrame({
-        "id": [1, 2],
-        "first_name": ["Kiran", "Sachin"],
-        "last_name": ["Sreekumar", "Tendulkar"],
-        "age": [35, 45],
-        "task_id": [1, 1],
-        "ingested_at": [now, now]
-    })
-    # ASSERT 
-    pd.testing.assert_frame_equal(left=expected_output_df,right=output_df_as_pd, check_exact=True, check_dtype=False)
